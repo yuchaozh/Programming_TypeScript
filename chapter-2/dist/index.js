2d62d5838d8e710058e6fa1ff507b2aaf689c59d
Yuchao Zhou <yuczho@microsoft.com>
2019-08-14 23:22:48 -0700

    updates

--- chapter-2/dist/index.js
+++ chapter-2/dist/index.js
@@ -1,3 +1,14 @@
 "use strict";
 console.log("Hello TypeScript!");
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+    apple: a,
+    banana: b
+};
+let d = c.apple * 4;
+function squareOf(n) {
+    return n * n;
+}
+console.log(squareOf(2));
 //# sourceMappingURL=index.js.map
